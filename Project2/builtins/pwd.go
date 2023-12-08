1e19e975f4ee8290aa34bf683aab93c3caa9f560
KhoaLe992 <128986222+KhoaLe992@users.noreply.github.com>
2023-12-07 23:58:59 -0600

    Create pwd.go

---
+++ Project2/builtins/pwd.go
@@ -0,0 +1,17 @@
+package builtins
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+// PrintWorkingDirectory prints the current working directory.
+func PrintWorkingDirectory(w io.Writer) error {
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	_, err = fmt.Fprintln(w, wd)
+	return err
+}
