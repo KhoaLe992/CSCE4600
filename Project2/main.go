8473c96bf57224de436d235cc1d078cc29e29aed
KhoaLe992 <128986222+KhoaLe992@users.noreply.github.com>
2023-12-07 22:09:19 -0600

    Update main.go

--- Project2/main.go
+++ Project2/main.go
@@ -12,6 +12,61 @@ import (
 	"github.com/KhoaLe992/CSCE4600/Project2/builtins"
 )
 
+// Echo prints the specified text to the console.
+func Echo(w io.Writer, args ...string) error {
+	text := strings.Join(args, " ")
+	_, err := fmt.Fprintln(w, text)
+	return err
+}
+
+// PrintWorkingDirectory prints the current working directory.
+func PrintWorkingDirectory(w io.Writer) error {
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	_, err = fmt.Fprintln(w, wd)
+	return err
+}
+
+// ListDirectory lists the files and directories in the specified directory.
+func ListDirectory(w io.Writer, args ...string) error {
+	dir := "."
+	if len(args) > 0 {
+		dir = args[0]
+	}
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		_, err := fmt.Fprintln(w, file.Name())
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// MakeDirectory creates a new directory with the specified name.
+func MakeDirectory(args ...string) error {
+	if len(args) < 1 {
+		return fmt.Errorf("mkdir: missing operand")
+	}
+	dir := args[0]
+	return os.Mkdir(dir, os.ModePerm)
+}
+
+// WhoAmI prints the current username.
+func WhoAmI(w io.Writer) error {
+	u, err := user.Current()
+	if err != nil {
+		return err
+	}
+	_, err = fmt.Fprintln(w, u.Username)
+	return err
+}
+
 func main() {
 	exit := make(chan struct{}, 2) // buffer this so there's no deadlock.
 	runLoop(os.Stdin, os.Stdout, os.Stderr, exit)
@@ -81,6 +136,16 @@ func handleInput(w io.Writer, input string, exit chan<- struct{}) error {
 	case "exit":
 		exit <- struct{}{}
 		return nil
+	case "echo":
+		return builtins.Echo(w, args...)
+	case "pwd":
+		return builtins.PrintWorkingDirectory(w)
+	case "ls":
+		return builtins.ListDirectory(w, args...)
+	case "mkdir":
+		return builtins.MakeDirectory(args...)
+	case "whoami":
+		return builtins.WhoAmI(w)
 	}
 
 	return executeCommand(name, args...)
